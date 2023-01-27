825337c530a49aa2c5dd58f8e212c1e18e7c29eb
Parisa78 <parisa_alaie@comp.iust.ac.ir>
2023-01-28 02:05:41 +0330

    fix merge package

--- heuristics/merge_package_heuristic.py
+++ heuristics/merge_package_heuristic.py
@@ -1,31 +1,83 @@
-from org.antlr.v4.runtime import ANTLRFileStream
-from org.antlr.v4.runtime import CommonTokenStream
-from JavaLexer import JavaLexer
-from JavaParser import JavaParser
+from statistics import mean
 
-# Create the lexer and parser
-input_stream = ANTLRFileStream("C:\Users\mr.ranjbar\PycharmProjects\DEPISTER\source")
-lexer = JavaLexer(input_stream)
-tokens = CommonTokenStream(lexer)
-parser = JavaParser(tokens)
 
-# Parse the compilation unit
-cu = parser.compilationUnit()
+# def get_uniq_packages(package_classes_dict):
+#     first_appear = False
+#     package_classes_list = list(package_classes_dict.keys())
+#     chosen_package_list = []
+#     temp = [package_classes_list[0], package_classes_dict[package_classes_list[0]]]
+#     for first_package in package_classes_list:
+#         for second_package in package_classes_list:
+#             if first_package in second_package and first_package != second_package:
+#                 first_appear = True
+#                 if len(first_package) <= len(temp[0]):
+#                     temp = [first_package, package_classes_dict[first_package]]
+#
+#             if not (first_package in second_package) and first_package != second_package:
+#                 if len(second_package) <= len(first_package):
+#                     chosen_package_list.append([second_package, package_classes_dict[second_package]])
+#                     first_appear=True
+#         if not first_appear:
+#             first_appear=False
+#             chosen_package_list.append([first_package, package_classes_dict[first_package]])
+#     chosen_package_list.append(temp)
+#
+#     return chosen_package_list
 
-# Find the class that contains the method
-class_ = cu.classDeclaration(name="SourceClass")
 
-# Find the method to move
-method = class_.methodDeclaration(name="methodToMove")
+def calculate_avg(package_classes_dict):
+    # get_mean=mean(package_classes_dict)
+    return mean(package_classes_dict)
+    # chosen_package_list = get_uniq_packages(package_classes_dict)
+    # return mean([item[1] for item in chosen_package_list])
 
-# Create the target class
-target_class = cu.classDeclaration(name="TargetClass")
 
-# Add the method to the target class
-target_class.methods().add(method)
+def choose_packages(package_classes_dict):
+    # value=list(package_classes_dict.values())
+    average = int(calculate_avg(list(package_classes_dict.values())))
+    chosen_list = []
 
-# Remove the method from the source class
-class_.methods().remove(method)
+    # chosen_package_list = get_uniq_packages(package_classes_dict)
+    # for item in chosen_package_list:
+    #     if item[1] < average:
+    #         chosen_list.append(item)
 
-# Print the modified Java source code
-print(cu.toStringTree())
+    for key, value in package_classes_dict.items():
+        if value < average:
+            chosen_list.append([key, value])
+
+    return chosen_list[:2]
+
+
+def merge_packages(package_classes_dict, directory):
+    packages_list = choose_packages(package_classes_dict)
+
+    first_file_path = directory + 'java/' + packages_list[0][0].replace('.', '/') + '/'
+    second_file_path = directory + 'java/' + packages_list[1][0].replace('.', '/') + '/'
+    first_file = open(first_file_path, 'r')
+    second_file = open(second_file_path, 'w')
+
+    second_file_context = ''
+    contexts = ''
+    import_sets = set()
+    for line in second_file.readlines():
+        if 'package ' + packages_list[1][0] + ';' in line:
+            second_file_context += line
+        elif 'import' in line:
+            import_sets.add(line)
+        else:
+            contexts += line
+
+    for line in first_file.readlines():
+        if 'package ' + packages_list[0][0] + ';' in line:
+            continue
+        elif 'import' in line:
+            import_sets.add(line)
+        else:
+            contexts += line
+
+    for x in import_sets:
+        second_file_context += x
+
+    second_file_context += contexts
+    second_file.write(second_file_context)
--- heuristics/my_listener.py
+++ heuristics/my_listener.py
@@ -1,8 +1,8 @@
 from antlr4 import CommonTokenStream
 from antlr4.TokenStreamRewriter import TokenStreamRewriter
 
-from gen.javaLabeled import JavaParserLabeled
-from gen.javaLabeled import JavaParserLabeledListener
+from gen.javaLabeled.JavaParserLabeled import JavaParserLabeled
+from gen.javaLabeled.JavaParserLabeledListener import JavaParserLabeledListener
 
 
 class MyListener(JavaParserLabeledListener):
@@ -10,14 +10,13 @@ class MyListener(JavaParserLabeledListener):
     To detect whether class needs refactoring or not.
     """
 
-    def __init__(self, common_token_stream: CommonTokenStream = None, package_classes_dict=None,class_method_dict=None):
+    def __init__(self, common_token_stream: CommonTokenStream = None, package_classes_dict=None):
         """
         :param common_token_stream:
         """
         self.token_stream = common_token_stream
         self.method_count = 0
         self.package_classes_dict = package_classes_dict
-        self.class_method_dict=class_method_dict
         self.package_name = None
 
         # Move all the tokens in the source code in a buffer, token_stream_rewriter.
@@ -30,9 +29,6 @@ class MyListener(JavaParserLabeledListener):
     def exitMethodDeclaration(self, ctx: JavaParserLabeled.MethodDeclarationContext):
         self.method_count = self.method_count + 1
 
-    # def enterMethodDeclaration(self, ctx: JavaParserLabeled.MethodDeclarationContext):
-    #     self.class_methods_dict[self.class_name][self.method_name].append(ctx.IDENTIFIER().getText())
-
     # Exit a parse tree produced by JavaParserLabeled#classDeclaration.
     def exitClassDeclaration(self, ctx: JavaParserLabeled.ClassDeclarationContext):
         self.package_classes_dict[self.package_name] += 1
@@ -42,36 +38,3 @@ class MyListener(JavaParserLabeledListener):
         self.package_name = ctx.qualifiedName().getText()
         if self.package_name not in self.package_classes_dict:
             self.package_classes_dict[self.package_name] = 0
-
-
-    # def enterMethodCall(self,ctx: JavaParserLabeled.ClassDeclarationContext):
-    #     self.method_name=ctx.IDENTIFIER().getText()
-    #     if self.method_name not in self.class_methods_dict:
-    #         self.class_methods_dict[self.method_name]=[]
-
-#########for huristic move method
-
-    def enterMethodDeclaration(self, ctx: JavaParserLabeled.MethodDeclarationContext): ##method 2
-        self.package_classes_dict[self.package_name][self.class_name].append(ctx.IDENTIFIER().getText())
-
-
-    # Exit a parse tree produced by JavaParserLabeled#classDeclaration.
-    # def enterClassDeclaration(self, ctx: JavaParserLabeled.ClassDeclarationContext): ###method1
-    #     self.class_name=ctx.IDENTIFIER().getText() #######IDENTIFIER
-    #
-    #     self.package_classes_dict[self.package_name] [self.class_name]=[]
-    def enterMethodDeclaration(self, ctx: JavaParserLabeled.ClassDeclarationContext): ###method1
-        self.method_name=ctx.IDENTIFIER().getText() #######IDENTIFIER
-
-        self.class_method_dict[self.class_name] [self.method_name]=[]
-
-    # Enter a parse tree produced by JavaParserLabeled#packageDeclaration.
-    # def enterPackageDeclaration(self, ctx: JavaParserLabeled.PackageDeclarationContext): ##class
-    #     self.package_name = ctx.qualifiedName().getText()
-    #     if self.package_name not in self.package_classes_dict:
-    #         self.package_classes_dict[self.package_name] = {}
-
-    def enterClassDeclaration(self, ctx: JavaParserLabeled.PackageDeclarationContext): ##class
-        self.class_name=ctx.IDENTIFIER().getText()
-        if self.class_name not in self.class_method_dict:
-            self.class_method_dict[self.class_name] = {}
