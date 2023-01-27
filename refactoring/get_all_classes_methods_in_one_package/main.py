74600748fb6509ef2fc49f8349e94599f82aaa9c
Parisa78 <parisa_alaie@comp.iust.ac.ir>
2023-01-27 22:37:59 +0330

    add get methods and classes of one package

---
+++ refactoring/get_all_classes_methods_in_one_package/main.py
@@ -0,0 +1,74 @@
+from antlr4 import *
+
+import argparse
+import os
+
+# from merge_package_heuristic import choose_packages
+from my_listener import MyListener
+from codart.gen.javaLabeled.JavaLexer import JavaLexer
+from codart.gen.javaLabeled.JavaParserLabeled import JavaParserLabeled
+
+package_classes_dict = {}
+directory = 'src/ganttproject-master/biz.ganttproject.core/src/main'
+
+
+def main(args):
+    global package_classes_dict
+
+    # Step 1: Load input source into stream
+    stream = FileStream(args.file, encoding='utf8')
+    # Step 2: Create an instance of AssignmentStLexer
+    lexer = JavaLexer(stream)
+    # Step 3: Convert the input source into a list of tokens
+    token_stream = CommonTokenStream(lexer)
+    # Step 4: Create an instance of the AssignmentStParser
+    parser = JavaParserLabeled(token_stream)
+    parser.getTokenStream()
+    # Step 5: Create parse tree
+    parse_tree = parser.compilationUnit()
+    # Step 6: Create an instance of AssignmentStListener
+    my_listener = MyListener(common_token_stream=token_stream, package_classes_dict=package_classes_dict) ####shaghayegh
+    walker = ParseTreeWalker()
+    walker.walk(t=parse_tree, listener=my_listener)
+
+    return  my_listener.package_classes_dict
+    # print(args.file, '=>', my_listener.method_count)
+
+
+def recursive_walk(directory):
+    for dirname, dirs, files in os.walk(directory):
+        for filename in files:
+            filename_without_extension, extension = os.path.splitext(filename)
+            if extension == '.java':
+                process_file("{}/{}".format(dirname, filename))
+
+
+def process_file(file):
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        '-n', '--file',
+        help='Input source', default=file)
+
+    args = argparser.parse_args()
+    return main(args)
+
+
+# if __name__ == '__main__':
+#     recursive_walk(directory)
+#
+#     for first_package in package_classes_dict:
+#         for second_package in package_classes_dict:
+#             if first_package in second_package and first_package != second_package:
+#                 package_classes_dict[first_package] += package_classes_dict[second_package]
+#
+#     for package, num_of_classes in package_classes_dict.items():
+#         print(package, '=>', num_of_classes)
+#
+#     # print('Less than average p1 & p2:')
+#     # print(choose_packages(package_classes_dict))
+    # merge_packages(package_classes_dict, directory)
+if __name__ == '__main__':
+    # from codart.utility.directory_utils import update_understand_database
+
+    packages_dict=process_file('D:/univercity/Term 1 Arshad/Architecture software systems/CodART/tests/move_method/s.java')
+    print(packages_dict)
---
+++ refactoring/get_all_classes_methods_in_one_package/my_listener.py
@@ -0,0 +1,44 @@
+from antlr4 import CommonTokenStream
+from antlr4.TokenStreamRewriter import TokenStreamRewriter
+
+from codart.gen.javaLabeled.JavaParserLabeled import JavaParserLabeled
+from codart.gen.javaLabeled.JavaParserLabeledListener import JavaParserLabeledListener
+
+
+class MyListener(JavaParserLabeledListener):
+    """
+    To detect whether class needs refactoring or not.
+    """
+
+    def __init__(self, common_token_stream: CommonTokenStream = None, package_classes_dict=None):
+        """
+        :param common_token_stream:
+        """
+        self.token_stream = common_token_stream
+        # self.method_count = 0
+        self.package_classes_dict = package_classes_dict
+        self.package_name = None
+
+        # Move all the tokens in the source code in a buffer, token_stream_rewriter.
+        if common_token_stream is not None:
+            self.token_stream_rewriter = TokenStreamRewriter(common_token_stream)
+        else:
+            raise TypeError('common_token_stream is None')
+
+    # Enter a parse tree produced by JavaParserLabeled#methodDeclaration.
+    def enterMethodDeclaration(self, ctx: JavaParserLabeled.MethodDeclarationContext):
+        self.package_classes_dict[self.package_name][self.class_name].append(ctx.IDENTIFIER().getText())
+
+
+    # Exit a parse tree produced by JavaParserLabeled#classDeclaration.
+    def enterClassDeclaration(self, ctx: JavaParserLabeled.ClassDeclarationContext):
+        self.class_name=ctx.IDENTIFIER().getText() #######IDENTIFIER
+
+        self.package_classes_dict[self.package_name] [self.class_name]=[]
+
+
+    # Enter a parse tree produced by JavaParserLabeled#packageDeclaration.
+    def enterPackageDeclaration(self, ctx: JavaParserLabeled.PackageDeclarationContext):
+        self.package_name = ctx.qualifiedName().getText()
+        if self.package_name not in self.package_classes_dict:
+            self.package_classes_dict[self.package_name] = {}
